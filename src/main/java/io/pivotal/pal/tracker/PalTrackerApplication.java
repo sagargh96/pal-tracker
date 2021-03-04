f9dd32658d08c67995db2169c5480e4a7d1425a7
Ubuntu <ubuntu@ip-172-31-0-235.ap-southeast-1.compute.internal>
2021-03-04 05:48:00 +0000

    sng9 - added JBC configuration

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
-    @Bean
-    public TimeEntryRepository getRepository(){
-        return new InMemoryTimeEntryRepository();
-    }
+//    @Bean
+//    public TimeEntryRepository getRepository(){
+//        return new InMemoryTimeEntryRepository();
+//    }
+@Bean
+public TimeEntryRepository getRepository(DataSource dataSource){
+    return new JdbcTimeEntryRepository(dataSource);
+}
 
 }
